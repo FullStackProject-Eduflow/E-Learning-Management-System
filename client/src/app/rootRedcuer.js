54cb803729b3ce6f3297a20daaca6c00e5134360
Ayush Dhoundiyal <ayushdhoundiyal@Ayushs-MacBook-Air.local>
2025-03-18 00:41:07 -0400

    This is the comit from my side connecting the login pages and cadding cotainers, making some Ui changes

--- client/src/app/rootRedcuer.js
+++ client/src/app/rootRedcuer.js
@@ -1,9 +1,10 @@
-import authReducer from "./features/authSlice";
 import { combineReducers } from "@reduxjs/toolkit";
+import authReducer from "../features/authSlice";
 import { authApi } from "@/features/api/authApi";
 
-const rootReducer = combineReducers({
-    auth: authReducer,
+const rootRedcuer = combineReducers({
     [authApi.reducerPath]: authApi.reducer,
+    auth:authReducer
 });
-export default rootReducer;
+
+export default rootRedcuer;
--- server/routes/user.route.js
+++ server/routes/user.route.js
@@ -1,17 +1,8 @@
 import express from 'express';
-import { register, login } from '../controllers/user.controller.js'; // ✅ Import register & login
-
+import { login, register } from '../controllers/user.controller.js';
 const router = express.Router();
 
 router.route("/register").post(register);
 router.route("/login").post(login);
 
-router.get("/", (req, res) => {
-    res.status(200).json({
-        success: true,
-        message: "User route is working!"
-    });
-});
-
-// ✅ Export the router
 export default router;
