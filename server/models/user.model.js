db9d40434417c2bcbe917e1192c66218e621ae8c
Ayush Dhoundiyal <ayushdh96@gmail.com>
2025-03-18 15:41:54 -0400

    Revert "This is the comit from my side connecting the login pages and cotainers, making some Ui changes"

--- server/models/user.model.js
+++ server/models/user.model.js
@@ -1,5 +1,7 @@
+//we bring mongoose to it 
+//db connect karne ke baad we create a schema to see how user data will be 
+//nstall mongoose 
 import mongoose from "mongoose";
-
 const userSchema = new mongoose.Schema({
     name: {
         type: String,
@@ -7,28 +9,34 @@ const userSchema = new mongoose.Schema({
     },
     email: {
         type: String,
-        required: true
+        required: true,
+        unique: true
     },
     password: {
         type: String,
         required: true
     },
-    role:
-    {
+    role: {
         type: String,
-        enum:["instructor","student"],
-        default:"student"
+        required: true,
+        // admin or user so enum is used when we v few vals and we need 1 value only 
+        enum: ['instructor', 'student'],
+        default: 'student'
     },
-    enrolledCourses:[
-        {
-            type: mongoose.Schema.Types.ObjectId,
-            ref:"Course"
-        }
-    ],
-    photoUrl:{
-        type:String,
-        default:""
-    }
-},{timestamps:true});
+    enrolledCourses: [{
+        type: mongoose.Schema.Types.ObjectId,
+        ref: 'Course'
+    }],
+    photoUrl: {
+        type: String,
+        default:" "
 
-export const User=mongoose.model("User",userSchema);
+    }
+},
+
+    
+ {
+    timestamps: true
+});
+const User = mongoose.model("User", userSchema);
+export default User;
