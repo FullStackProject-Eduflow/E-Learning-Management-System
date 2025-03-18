d2956759bda87bda7b16c40669ccc1abff374181
Ayush Dhoundiyal <ayushdhoundiyal@mac.notrust.clients.wireless.pfw.edu>
2025-03-18 15:51:41 -0400

    This is the comit from my side connecting the login pages and adding cotainers, making some UI changes

--- client/src/features/authSlice.js
+++ client/src/features/authSlice.js
@@ -1,24 +1,23 @@
 import { createSlice } from "@reduxjs/toolkit";
 
 const initialState = {
-    isAuthenticated: false,
     user: null,
+    isAuthenticated: false,
 };
 
 const authSlice = createSlice({
-    name: "auth",
+    name:"authSlice",
     initialState,
     reducers: {
         userLoggedIn: (state, action) => {
             state.user = action.payload.user;
-            state.isAuthenticated = true;
-        },
-        userLoggedOut: (state) => {
-            state.user = null;
-            state.isAuthenticated = false;
-        },
     },
+    userLoggedOut: (state) => {
+        state.user = null;
+        state.isAuthenticated = false;
+    }
+},
 });
 
 export const { userLoggedIn, userLoggedOut } = authSlice.actions;
-export default authSlice.reducer; 
+export default authSlice.reducer;
