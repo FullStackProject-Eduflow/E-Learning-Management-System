b9759842da7a58c628d7c6c6a09c4a97c6fb12ab
Prudhvi Sairam Chundru <chunp01@pfw.edu>
2025-03-20 10:21:39 -0400

    Common Files Changes to support the whole Functiionality

--- server/index.js
+++ server/index.js
@@ -4,6 +4,9 @@ import cookieParser from 'cookie-parser';
 import cors from 'cors';
 import connectDB from './database/db.js';
 import userRoute from './routes/user.route.js';
+import courseRoute from './routes/course.route.js';
+import mediaRoute from "./routes/media.route.js";
+
 
 dotenv.config({});
 //call database connection
@@ -21,8 +24,9 @@ app.use(cors({
   credentials:true
 }));
 //apis port http://localhost:8080/api/v1/user/register
-
+app.use("/api/v1/media", mediaRoute);
 app.use("/api/v1/user", userRoute);
+app.use("/api/v1/course", courseRoute);
 
 
 app.listen(PORT, () => {
