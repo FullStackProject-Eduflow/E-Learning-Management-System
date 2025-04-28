47154c94c10ab11b40f23ad72172f2586442e3b7
ayushdh96 <ayushdh96@gmail.com>
2025-04-27 20:09:51 -0400

    cookies are getting svaed, so checking that

--- server/middlewares/isAuthenticated.js
+++ server/middlewares/isAuthenticated.js
@@ -4,16 +4,16 @@ const isAuthenticated = async (req, res, next) => {
   try {
     const token = req.cookies.token;
     if (!token) {
-      return res.status(200).json({
-        message: "User authenticated",
-        success: true,
+      return res.status(401).json({
+        message: "User not authenticated",
+        success: false,
       });
     }
     const decode = await jwt.verify(token, process.env.SECRET_KEY);
     if (!decode) {
-      return res.status(200).json({
-        message: "Valid token",
-        success: true,
+      return res.status(401).json({
+        message: "Invalid token",
+        success: false,
       });
     }
     req.id = decode.userId;
