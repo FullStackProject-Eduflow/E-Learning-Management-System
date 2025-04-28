9b9aae0ebe1a3b3f6ed45f9109c66d76c6a69f4f
ayushdh96 <ayushdh96@gmail.com>
2025-04-27 21:25:01 -0400

    payment issue correction with the url change

--- server/controllers/coursePurchase.controller.js
+++ server/controllers/coursePurchase.controller.js
@@ -40,8 +40,8 @@ export const createCheckoutSession = async (req, res) => {
         },
       ],
       mode: "payment",
-      success_url: `https://e-learning-management-system-w9mi.onrender.com/course-progress/${courseId}`,
-      cancel_url: `https://e-learning-management-system-w9mi.onrender.com/course-detail/${courseId}`,
+      success_url: `https://e-learning-management-system-1.onrender.com/course-progress/${courseId}`,
+      cancel_url: `https://e-learning-management-system-1.onrender.com/course-detail/${courseId}`,
       metadata: {
         courseId: courseId,
         userId: userId,
